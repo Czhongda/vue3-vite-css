596c4166a4e27b2221ebcc00ce481d01ff3d8ffe
kmchen <kmchen@suntekcorps.com>
2023-09-07 16:41:56 +0800

    dev- push

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/gridTab">网格grid</RouterLink>
         <RouterLink to="/flexTab">flex布局</RouterLink>
-        <RouterLink to="/refTem">模板引用</RouterLink>
+        <RouterLink to="/refTem">模板引用-dev-push</RouterLink>
         <RouterLink to="/compList">模板1</RouterLink>
       </nav>
     </div>
