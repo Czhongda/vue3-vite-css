3fe924910f3c156d62d54997ce001704e549a62b
kmchen <kmchen@suntekcorps.com>
2023-09-12 14:22:32 +0800

    获取自定义属性：getAttribute

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+app.directive('focus', {
+  mounted(el, binding, vnode, prevVnode) {
+    // console.log(binding.value.color) // => "white"
+    console.log('绑定','el, binding, vnode, prevVnode',el,el.type,el.id,el.dataset, binding, vnode, prevVnode);
+  }
+})
 // app.component('countTab', countTab).component('countTab2', countTab2)
 
 app.use(createPinia())
--- src/views/customDirec/index.vue
+++ src/views/customDirec/index.vue
@@ -1,26 +1,26 @@
 <template>
   <div>
     customDirection
-    input: <input v-focus ref="inputRef" type="text" v-model="inputValue" id="1222" data-id="1234567890" data-user="carinaanand" data-date-of-birth />
+    input: <input v-focus ref="inputRef" color="blue" type="text" v-model="inputValue" id="1222" data-id="1234567890" data-user="carinaanand" data-date-of-birth />
   </div>
 </template>
 <script setup lang="ts">
 import {ref,onMounted} from 'vue'
 const inputRef = ref(null)
 const inputValue  = ref('')
-const vFocus = {
-  mounted: (el, binding, vnode, prevVnode) => {
-    el.focus()
-    console.log('el, binding, vnode, prevVnode',el, binding,888, vnode.value, prevVnode);
-  }
-}
+// const vFocus = {
+//   mounted: (el, binding, vnode, prevVnode) => {
+//     el.focus()
+//     console.log('el, binding, vnode, prevVnode',el, binding,888, vnode.value, prevVnode);
+//   }
+// }
 onMounted(() => {
   inputValue.value = 'carinaanand'
 })
 
 setTimeout(() => {
   inputRef.value.id='user'
-  console.log('测试dataset',inputRef.value.value);
+  console.log('测试dataset',inputRef.value.value,inputRef.value.id,inputRef.value.type,inputRef.value.getAttribute("color"));
 }, 3000);
 
 </script>
